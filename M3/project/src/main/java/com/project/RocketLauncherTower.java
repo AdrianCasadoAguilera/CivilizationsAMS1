30bf9fc078ebdcd391cf208e933b69c4e350b3ec
OscarMG018 <108469203+OscarMG018@users.noreply.github.com>
2024-05-03 18:44:40 +0200

    RocketLuncherTower complete

---
+++ M3/project/src/main/java/com/project/RocketLauncherTower.java
@@ -0,0 +1,72 @@
+package com.project;
+
+public class RocketLauncherTower extends DefenseUnit{
+
+    public RocketLauncherTower() {
+        this.armor = (int)(ARMOR_ROCKETLAUNCHERTOWER * (1+Civilization.getInstance().getTechnologyDefense()*PLUS_ARMOR_ROCKETLAUNCHERTOWER_BY_TECHNOLOGY/100.0));
+        this.initialArmor = armor;
+        this.baseDamage = (int)(BASE_DAMAGE_ROCKETLAUNCHERTOWER * (1+Civilization.getInstance().getTechnologyDefense()*PLUS_ATTACK_ROCKETLAUNCHERTOWER_BY_TECHNOLOGY/100.0));
+    }
+
+    @Override
+    public int attack() {
+        return (int)(baseDamage+baseDamage*experience*PLUS_ATTACK_UNIT_PER_EXPERIENCE_POINT/100);
+    }
+
+    @Override
+    public int getActualArmor() {
+        return (int)(armor+initialArmor*experience*PLUS_ARMOR_UNIT_PER_EXPERIENCE_POINT/100);
+    }
+
+    @Override
+    public int getChanceAttackAgain() {
+        return CHANCE_ATTACK_AGAIN_ROCKETLAUNCHERTOWER;
+    }
+
+    @Override
+    public int getChanceGeneratinWaste() {
+        return CHANCE_GENERATNG_WASTE_ROCKETLAUNCHERTOWER;
+    }
+
+    @Override
+    public int getExperience() {
+        return experience;
+    }
+
+    @Override
+    public int getFoodCost() {
+       return FOOD_COST_ROCKETLAUNCHERTOWER;
+    }
+
+    @Override
+    public int getIronCost() {
+        return IRON_COST_ROCKETLAUNCHERTOWER;
+    }
+
+    @Override
+    public int getManaCost() {
+        return MANA_COST_ROCKETLAUNCHERTOWER;
+    }
+
+    @Override
+    public int getWoodCost() {
+        return WOOD_COST_ROCKETLAUNCHERTOWER;
+    }
+
+    @Override
+    public void resetArmor() {
+        armor = initialArmor;
+    }
+
+    @Override
+    public void setExperience(int exp) {
+        experience = exp;
+    }
+    @Override
+    public void takeDamage(int receivedDamage) {
+        armor -= receivedDamage;
+    } 
+    }
+
+     
+}
