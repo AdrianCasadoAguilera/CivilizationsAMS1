ae1f51352b3af685a39f48fb53c28f57c8638f4f
AdrianCasadoAguilera <acasadoag2@gmail.com>
2024-05-03 17:06:41 +0200

    Military units created

---
+++ M3/project/src/main/java/com/project/AttackUnit.java
@@ -0,0 +1,9 @@
+package com.project;
+
+public abstract class AttackUnit implements Variables,MilitaryUnit {
+    private int armor;
+    private int initialArmor;
+    private int baseDamage;
+    private int experience;
+    private boolean sanctified;
+}
