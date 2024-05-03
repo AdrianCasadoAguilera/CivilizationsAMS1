d4ba2b525928d15724ebb8f8ee8a4f75927361d3
OscarMG018 <108469203+OscarMG018@users.noreply.github.com>
2024-05-03 18:58:00 +0200

    CreateBattle

---
+++ M3/project/src/main/java/com/project/Battle.java
@@ -0,0 +1,9 @@
+package com.project;
+
+import java.util.ArrayList;
+
+public class Battle {
+    ArrayList<MilitaryUnit> civilizationArmy;
+    ArrayList<MilitaryUnit> enemyArmy;
+    ArrayList<ArrayList<>> armies
+}
