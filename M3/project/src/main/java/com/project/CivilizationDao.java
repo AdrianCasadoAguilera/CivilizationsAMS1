91eb836439c9f7c84fb6224c4817f0c12742b867
OscarMG018 <108469203+OscarMG018@users.noreply.github.com>
2024-05-13 19:03:50 +0200

    dao progress

--- M3/project/src/main/java/com/project/CivilizationDao.java
+++ M3/project/src/main/java/com/project/CivilizationDao.java
@@ -1,6 +1,10 @@
 package com.project;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.sql.*;
 
 public class CivilizationDao {
 
@@ -9,7 +13,43 @@ public class CivilizationDao {
     }
 
     public ArrayList<SaveData> getSaves() {
-        return new ArrayList<>();
+        ArrayList<SaveData> saves = new ArrayList<>();
+        AppData db = AppData.getInstance();
+
+        // Call the SQL function
+        List<Map<String, Object>> result = db.query("SELECT F_civilizations_stats_quantity(civilization_id) as values FROM civilization_stats");
+
+        // Process the result
+        if (!result.isEmpty()) {
+            for (Map<String, Object> row : result) {
+          
+                Array array = (Array) row.get("values"); 
+
+                // Convert the VARRAY to a Java list
+                List<Number> numberList = convertArrayToList(array);
+                SaveData save = new SaveData();
+                save.setWood(numberList.get(0).intValue());
+                //TODO the rest
+                saves.add(save);
+            }
+            
+        }
+        return saves;
+    }
+
+     private static List<Number> convertArrayToList(Array array) {
+        List<Number> list = new ArrayList<>();
+        try {
+            // Get the SQL array as a java.sql.Array
+            int[] types = array.getArrayTypeInfo();
+            int[] sizes = array.getArraySize();
+            for (int i = 0; i < sizes.length; i++) {
+                list.add((Number) array.getArray(types[i], i));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 
     public void updateSave(SaveData data) {
