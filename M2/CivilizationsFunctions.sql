d0357e9ac4ffe012725704628971408f319c185a
AAyoubelbakhti <ayoubelbak13@gmail.com>
2024-05-15 21:48:44 +0200

    Convertir  db a xml
    función en db

--- M2/CivilizationsFunctions.sql
+++ M2/CivilizationsFunctions.sql
@@ -14,3 +14,25 @@ BEGIN
  
     RETURN myArray; 
 END; 
+
+DECLARE
+    l_context    DBMS_XMLGEN.ctxHandle;
+    l_xml        CLOB;
+    l_table_name VARCHAR2(255);
+    l_sql        VARCHAR2(4000);
+    l_full_xml   CLOB := '<database>'; -- Inicializamos con la raíz del XML
+BEGIN
+    FOR table_record IN (SELECT table_name FROM user_tables) LOOP
+        l_table_name := table_record.table_name;
+        l_sql := 'SELECT * FROM ' || l_table_name;
+        l_context := DBMS_XMLGEN.newContext(l_sql);
+        DBMS_XMLGEN.setRowTag(l_context, l_table_name);
+        DBMS_XMLGEN.setRowSetTag(l_context, 'ROWSET');
+        l_xml := DBMS_XMLGEN.getXML(l_context);
+        DBMS_XMLGEN.closeContext(l_context);
+        l_full_xml := l_full_xml || '<' || l_table_name || '>' || l_xml || '</' || l_table_name || '>';
+    END LOOP;
+    l_full_xml := l_full_xml || '</database>';
+    DBMS_OUTPUT.put_line(l_full_xml);
+END;
+/
