7f239f6c333bd60372fa2e5cefc7bcdcc9361ff5
OscarMG018 <108469203+OscarMG018@users.noreply.github.com>
2024-05-16 20:15:29 +0200

    sql del dao

--- M2/CivilizationsSchema.sql
+++ M2/CivilizationsSchema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE civilization_stats ( 
-    civilization_id NUMBER PRIMARY KEY, 
+    civilization_id NUMBER PRIMARY KEY AUTO-INCREMENT, 
     name VARCHAR2(255), 
     wood_amount NUMBER NOT NULL, 
     iron_amount NUMBER NOT NULL, 
@@ -12,55 +12,43 @@ CREATE TABLE civilization_stats (
     carpentry_counter NUMBER NOT NULL, 
     technology_defence_level NUMBER, 
     technology_attack_level NUMBER, 
-    battles_counter NUMBER 
+    battles_counter NUMBER,
+    battle_timer NUMBER,
+    NextBattleIn NUMBER
 );
 
-CREATE TABLE attack_unit_stats ( 
-    attack_unit_id NUMBER PRIMARY KEY, 
+CREATE TABLE units ( 
+    unit_id NUMBER PRIMARY KEY, 
     civilization_id NUMBER NOT NULL, 
     type VARCHAR2(20), 
-    armor NUMBER, 
-    base_damage NUMBER, 
     experience NUMBER, 
-    sanctified CHAR(1), 
-    CONSTRAINT ck_type_unit_attack CHECK (type IN ('Swordsman', 'Spearman', 'Crossbow', 'Cannon')),
-  	CONSTRAINT fk_civilization_unit_attack
+    CONSTRAINT ck_type_unit CHECK (type IN ('Swordsman', 'Spearman', 'Crossbow', 'Cannon','Arrowtower','Rocketlaunchertower','Magician','Priest')),
+  	CONSTRAINT fk_civilization_unit
     	FOREIGN KEY (civilization_id)
     	REFERENCES civilization_stats(civilization_id)
 );
 
-CREATE TABLE special_unit_stats ( 
-    special_unit_id NUMBER PRIMARY KEY, 
+CREATE TABLE enemy_unit ( 
+    unit_id NUMBER PRIMARY KEY, 
     civilization_id NUMBER NOT NULL, 
     type VARCHAR2(20), 
-    armor NUMBER, 
-    base_damage NUMBER, 
     experience NUMBER, 
-    CONSTRAINT ck_type_unit_special CHECK (type IN ('Magician','Priest')),
-  	CONSTRAINT fk_civilization_unit_special
+    CONSTRAINT ck_type_enemy_unit CHECK (type IN ('Swordsman', 'Spearman', 'Crossbow', 'Cannon')),
+  	CONSTRAINT fk_enemy_unit
     	FOREIGN KEY (civilization_id)
     	REFERENCES civilization_stats(civilization_id)
 );
 
-CREATE TABLE defence_unit_stats ( 
-    defence_unit_id NUMBER PRIMARY KEY, 
-    civilization_id NUMBER NOT NULL, 
-    type VARCHAR2(20), 
-    armor NUMBER, 
-    base_damage NUMBER, 
-    experience NUMBER, 
-    sanctified CHAR(1), 
-    CONSTRAINT ck_type_unit_defence CHECK (type IN ('ArrowTower','Catapult','RocketLauncherTower') ),
-  	CONSTRAINT fk_civilization_unit_defence
-    	FOREIGN KEY (civilization_id)
-    	REFERENCES civilization_stats(civilization_id)
-);
+CREATE TYPE number_array AS VARRAY(4) OF NUMBER;
 
 CREATE TABLE battle_stats ( 
     civilization_id NUMBER NOT NULL, 
     num_battle NUMBER NOT NULL, 
     wood_acquired NUMBER, 
     iron_acquired NUMBER, 
+    win NUMBER CHECK (win IN (0, 1)),
+    civilizationLoses number_array,
+    EnemyLoses number_array,
     PRIMARY KEY(civilization_id, num_battle), 
   	CONSTRAINT fk_civilization_battle
     	FOREIGN KEY (civilization_id)
@@ -68,55 +56,28 @@ CREATE TABLE battle_stats (
 );
 
 
-CREATE TABLE civilization_attack_stats ( 
+CREATE TABLE civilization_unit_stats ( 
     civilization_id NUMBER NOT NULL, 
     num_battle NUMBER NOT NULL, 
     type VARCHAR2(20), 
     initial_attack NUMBER, 
     drops NUMBER, 
     PRIMARY KEY(civilization_id, num_battle, type), 
-    CONSTRAINT ck_type_attack CHECK (type IN ('Swordsman','Spearman','Crossbow','Cannon')  ),
-  	CONSTRAINT fk_civilization_attack
+    CONSTRAINT ck_type_unit_stats CHECK (type IN ('Swordsman','Spearman','Crossbow','Cannon')  ),
+  	CONSTRAINT fk_unit_stats
     	FOREIGN KEY (civilization_id, num_battle)
     	REFERENCES battle_stats(civilization_id, num_battle)
 );
 
-CREATE TABLE civilization_defense_stats ( 
-    civilization_id NUMBER NOT NULL, 
-    num_battle NUMBER NOT NULL, 
-    type VARCHAR2(20), 
-    initial_defence NUMBER, 
-    drops NUMBER, 
-    PRIMARY KEY(civilization_id, num_battle, type), 
-    CONSTRAINT ck_type_defence CHECK (type IN ('Magician','Priest')),
-  	CONSTRAINT fk_civilization_defence
-    	FOREIGN KEY (civilization_id, num_battle)
-    	REFERENCES battle_stats(civilization_id, num_battle)
-);
-
-
-CREATE TABLE civilization_special_stats ( 
-    civilization_id NUMBER NOT NULL, 
-    num_battle NUMBER NOT NULL, 
-    type VARCHAR2(20), 
-    initial_special NUMBER, 
-    drops NUMBER, 
-    PRIMARY KEY(civilization_id, num_battle, type), 
-    CONSTRAINT ck_type_special CHECK (type IN ('ArrowTower','Catapult','RocketLauncherTower') ),
-  	CONSTRAINT fk_civilization_special
-    	FOREIGN KEY (civilization_id, num_battle)
-    	REFERENCES battle_stats(civilization_id, num_battle)
-);
-
-CREATE TABLE enemy_attack_stats ( 
+CREATE TABLE enemy_unit_stats ( 
     civilization_id NUMBER NOT NULL, 
     num_battle NUMBER NOT NULL, 
     type VARCHAR2(20), 
     initial_enemy_attack NUMBER, 
     drops NUMBER, 
     PRIMARY KEY(civilization_id, num_battle, type), 
-    CONSTRAINT ck_type_enemy_attack CHECK (type IN ('Swordsman','Spearman','Crossbow','Cannon')  ),
-  	CONSTRAINT fk_civilization_enemy_attack
+    CONSTRAINT ck_type_enemy_stats CHECK (type IN ('Swordsman','Spearman','Crossbow','Cannon')  ),
+  	CONSTRAINT fk_enemy_stats
     	FOREIGN KEY (civilization_id, num_battle)
     	REFERENCES battle_stats(civilization_id, num_battle)
 );
