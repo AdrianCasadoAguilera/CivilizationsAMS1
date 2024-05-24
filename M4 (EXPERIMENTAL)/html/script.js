c3715795063446c4f5f0af53f7d7115189f613ce
AAyoubelbakhti <ayoubelbak13@gmail.com>
2024-05-24 16:46:00 +0200

    js

--- M4 (EXPERIMENTAL)/html/script.js
+++ M4 (EXPERIMENTAL)/html/script.js
@@ -1,14 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('run-script-button').addEventListener('click', function() {
-        fetch('/run-script')
-            .then(response => response.json())
-            .then(data => {
-                if (data.status === 'success') {
-                    console.log(data.message);
-                } else {
-                    console.error(data.message);
-                }
-            })
-            .catch(error => console.error('Error:', error));
-    });
+    fetch('/run-script')
+        .then(response => response.json())
+        .then(data => {
+            if (data.status === 'success') {
+                console.log(data.message);
+            } else {
+                console.error(data.message);
+            }
+        })
+        .catch(error => console.error('Error:', error));
 });
